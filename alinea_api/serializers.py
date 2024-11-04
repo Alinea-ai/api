cfaa47942fc39265ce48b68e11c720b483fb2561
pabloschall <pabloschall@gmail.com>
2024-11-03 17:24:19 -0700

    add serializers

---
+++ alinea_api/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import (
+    Entity,
+    AccessRequest,
+    AccessRequestItem,
+    UserPersonalInformation,
+    UserMedicalInfo,
+    DentalInfo,
+    PsychologicalInfo,
+    MedicalRecord,
+)
+
+class EntitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Entity
+        fields = '__all__'
+
+class AccessRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AccessRequest
+        fields = '__all__'
+
+class AccessRequestItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AccessRequestItem
+        fields = '__all__'
+
